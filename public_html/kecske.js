828b2892faf5fc5f25cdc225c61649a5d8000f7e
Gutyiii <gutyeszbandesz@gmail.com>
2021-04-07 11:12:55 +0200

    heaj

--- public_html/kecske.js
+++ public_html/kecske.js
@@ -9,36 +9,31 @@ function $(nev) {
 function init() {
 
 
-for (var i = 0; i < $("aside img").length; i++) {
-    $("#csonak div").style.backgroundImage ="kepek/csonak.png";
+    for (var i = 0; i < $("aside img").length; i++) {
+        $("#csonak div").style.backgroundImage = "kepek/csonak.png";
     }
-
 
     for (var i = 0; i < $("aside img").length; i++) {
 
         $("aside img")[i].addEventListener("click", feladat1);
     }
+
     for (var i = 0; i < $("aside img").length; i++) {
         $("img")[i].addEventListener("click", kepEltuntetesInnen);
         $("img")[i].addEventListener("click", kepEltunteteseCsonakbol);
     }
-    
+
     for (var i = 0; i < $("#tartalom1 div p").length; i++) {
-    $("#bal  p img")[i].addEventListener("mouseon", feladat5);
-      $("#bal p img")[i].addEventListener("mouseon", feladat6);    
+        $("#bal p img")[i].addEventListener("mouseon", feladat5);
+        $("#bal p img")[i].addEventListener("mouseon", feladat6);
     }
 }
-function feladat5(){
+function feladat5() {
     this.classlist.add("kiemel");
 }
-function feladat6(){
+function feladat6() {
     this.classlist.remove("kiemel");
 }
-
-
-   
-var tomb = {};
-
 
 function feladat1() {
     var tomb = {};
@@ -47,10 +42,11 @@ function feladat1() {
 }
 
 function kepEltuntetesInnen() {
-    for (var i = 0; i < $("img").length; i++) {
-        this.style.add("display:none");
-        $("article img")[i].style.remove("display:none");
+    for (var i = 0; i < max; i++) {
+        this.style.add("display:none")[i];
+        $("#csonak").add(this.img);
     }
+
 }
 
 
