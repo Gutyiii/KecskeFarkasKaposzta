cb3977f1d63e75ff0914a26c207b80f3465dc9ad
Hecz Klaudia <kicsihold21@gmail.com>
2021-04-07 11:12:22 +0200

    javitas

--- public_html/kecske.js
+++ public_html/kecske.js
@@ -7,13 +7,6 @@ function $(nev) {
     return document.querySelectorAll(nev);
 }
 function init() {
-
-
-for (var i = 0; i < $("aside img").length; i++) {
-    $("#csonak div").style.backgroundImage ="kepek/csonak.png";
-    }
-
-
     for (var i = 0; i < $("aside img").length; i++) {
 
         $("aside img")[i].addEventListener("click", feladat1);
@@ -22,21 +15,21 @@ for (var i = 0; i < $("aside img").length; i++) {
         $("img")[i].addEventListener("click", kepEltuntetesInnen);
         $("img")[i].addEventListener("click", kepEltunteteseCsonakbol);
     }
-    
-    for (var i = 0; i < $("#tartalom1 div p").length; i++) {
-    $("#bal  p img")[i].addEventListener("mouseon", feladat5);
-      $("#bal p img")[i].addEventListener("mouseon", feladat6);    
+
+    for (var i = 0; i < $("#bal p img").length; i++) {
+        $("#bal p img")[i].addEventListener("mouseover", feladat5);
+        $("#bal p img")[i].addEventListener("mouseout", feladat6);
     }
 }
-function feladat5(){
-    this.classlist.add("kiemel");
+function feladat5() {
+    this.classList.add("kiemel");
 }
-function feladat6(){
-    this.classlist.remove("kiemel");
+function feladat6() {
+    this.classList.remove("kiemel");
 }
 
 
-   
+
 var tomb = {};
 
 
@@ -55,6 +48,6 @@ function kepEltuntetesInnen() {
 
 
 function kepEltunteteseCsonakbol() {
-    
+
 }
 
